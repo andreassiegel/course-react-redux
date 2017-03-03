41455416d04f4b4dca284154cb70657f98f36aea
Andreas Siegel <andreas.siegel@smartrac-group.com>
2017-03-03 16:49:39 +0100

    Everything from the beginning to section 29

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'INSERT API KEY HERE';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new Component. This Component should produce some HTML.
+class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video = { this.state.selectedVideo } />
+        <VideoList
+          onVideoSelect = { selectedVideo => this.setState({ selectedVideo }) }
+          videos = { this.state.videos } />
+      </div>
+    );
+  }
+}
+
+// Take this Component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
