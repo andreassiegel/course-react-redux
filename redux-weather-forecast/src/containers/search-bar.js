26d5684a13e2bbf57dda3b5c6af583a7163d5f64
Andreas Siegel <andreas.siegel@smartrac-group.com>
2017-03-07 16:03:50 +0100

    Section 58: Redux Promise in Practice

--- redux-weather-forecast/src/containers/search-bar.js
+++ redux-weather-forecast/src/containers/search-bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
   constructor(props) {
     super(props);
@@ -19,6 +22,8 @@ export default class SearchBar extends Component {
     event.preventDefault();
 
     // We need to go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   render() {
@@ -37,3 +42,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
