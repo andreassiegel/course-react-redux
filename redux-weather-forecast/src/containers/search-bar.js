c5f629ad6d6c591e1cc5a9476b1a67dc66f9c777
Andreas Siegel <andreas.siegel@smartrac-group.com>
2017-03-07 15:13:31 +0100

    Section 54: Form Elements in React

--- redux-weather-forecast/src/containers/search-bar.js
+++ redux-weather-forecast/src/containers/search-bar.js
@@ -8,15 +8,22 @@ export default class SearchBar extends Component {
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // We need to go and fetch weather data
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={ this.onFormSubmit } className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
