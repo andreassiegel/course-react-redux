1c055c004788891a32f28dc2e76bc52b0997b823
Andreas Siegel <andreas.siegel@smartrac-group.com>
2017-03-09 14:18:17 +0100

    Section 65: Labeling of Units

--- redux-weather-forecast/src/components/chart.js
+++ redux-weather-forecast/src/components/chart.js
@@ -1,5 +1,10 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
 
 export default (props) => {
   return (
@@ -9,7 +14,9 @@ export default (props) => {
         width={ 180 }
         data={ props.data }>
         <SparklinesLine color={ props.color } />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{ average(props.data) } { props.units }</div>
     </div>
   );
 }
