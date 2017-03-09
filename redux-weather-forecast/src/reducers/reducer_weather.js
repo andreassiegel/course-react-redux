2f1596590524d7b9cff8f89ef6ba3186a4480c4b
Andreas Siegel <andreas.siegel@smartrac-group.com>
2017-03-09 13:42:21 +0100

    Section 62: Mapping Props to a Render Helper

--- redux-weather-forecast/src/reducers/reducer_weather.js
+++ redux-weather-forecast/src/reducers/reducer_weather.js
@@ -5,7 +5,7 @@ export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
       // return state.concat([action.payload.data]);
-      return [ action.payload.date, ...state ];
+      return [ action.payload.data, ...state ];
   }
 
   return state;
