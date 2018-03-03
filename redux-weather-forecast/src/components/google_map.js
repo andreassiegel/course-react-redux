baaf5bc0dcf99dcaf8856df15b79acd035bfb4e7
Andreas Siegel <mail@andreassiegel.de>
2018-03-03 14:43:25 +0100

    Section 68: Add Google Maps to the weather list

--- redux-weather-forecast/src/components/google_map.js
+++ redux-weather-forecast/src/components/google_map.js
@@ -5,10 +5,10 @@ class GoogleMap extends Component {
   componentDidMount() {
 
     new google.maps.Map(this.refs.map, {
-      zoom: 12,
+      zoom: 12, // city size on the screen
       center: {
         lat: this.props.lat,
-        lng: this.props.lng
+        lng: this.props.lon
       }
     });
   }
