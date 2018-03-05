19f0c57b9d0fa397efff571fc8a71d3fd6538f9d
Andreas Siegel <mail@andreassiegel.de>
2018-03-05 10:03:46 +0100

    Section 130: Fix React Router

--- redux-blog/src/index.js
+++ redux-blog/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
@@ -15,11 +15,11 @@ const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <div>
-        <Route path="/" component={ PostsIndex } />
-        <Route path="/posts/:id" component={ PostsShow } />
+      <Switch>
         <Route path="/posts/new" component={ PostsNew } />
-      </div>
+        <Route path="/posts/:id" component={ PostsShow } />
+        <Route path="/" component={ PostsIndex } />
+      </Switch>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
