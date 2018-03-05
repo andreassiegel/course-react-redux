201dc398601992b9c5f097f859cea2fb422fa867
Andreas Siegel <mail@andreassiegel.de>
2018-03-05 12:41:59 +0100

    Section 145: Receiving new posts

--- redux-blog/src/index.js
+++ redux-blog/src/index.js
@@ -7,8 +7,8 @@ import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
-import PostsShow from './components/posts_show';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
--- redux-blog/src/reducers/reducer_posts.js
+++ redux-blog/src/reducers/reducer_posts.js
@@ -1,13 +1,23 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS, CREATE_POST } from '../actions';
+import { FETCH_POSTS, CREATE_POST, FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');     
+        case FETCH_POST:
+            // ES 5:
+            // const post = action.payload.data;
+            // const newState = { ...state };
+            // newState[post.id] = post;
+            // return newState;
+
+            // ES 6:
+            const post = action.payload.data;
+            return { ...state, [post.id]: post };
         case CREATE_POST:
-            return state;       
+            return state;
         default:
             return state;
     }
