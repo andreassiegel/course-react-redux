511c8c8e1a1e7fc3d39a1d943dd72b0f0f669018
Andreas Siegel <mail@andreassiegel.de>
2018-03-05 11:28:46 +0100

    Section 138: Handling Form Submittal

--- redux-blog/src/components/posts_new.js
+++ redux-blog/src/components/posts_new.js
@@ -12,9 +12,16 @@ class PostsNew extends Component {
         );
     }
     
+    onSubmit(values) {
+        // this === component
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
                 <Field
                     label="Title"
                     name="title"
@@ -30,6 +37,7 @@ class PostsNew extends Component {
                     name="content"
                     component={ this.renderField }
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
